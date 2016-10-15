814b80dd5a41f64907e6b638b85629d66a667e15
Jason Graham <jason.graham@jhuapl.edu>
2016-10-14 21:18:21 -0400

    Added chk-backports-update.sh

---
+++ util/chk-backports-update.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Simple script to check packages in a BACKPORTS list file for updates 
+# between the backports git tree and the upstream git tree.
+#
+# Copyright (C) 2016
+#
+# Authors:
+#   Jason Graham <jgraha8@gmail.com>
+#
+
+function cleanup()
+{
+    rm -f .updated
+}
+
+if (( $# != 3 )); then
+    echo "Usage: `basename $0` backports-file backports-git-tree upstream-git-tree"
+    exit 1
+fi
+
+backports_file=$1
+backports_git_tree=$2
+upstream_git_tree=$3
+
+trap "cleanup; exit 1" SIGINT SIGTERM
+
+while read p
+do
+    for d in `find $backports_git_tree -name $p`
+    do
+	d_rel=${d#*/}
+	p_info="${d_rel}/${p}.info"
+
+	diff -u "${backports_git_tree}/${p_info}" "${upstream_git_tree}/${p_info}" > .updated
+	if [[ -s .updated ]]; then
+	    version_old=$(grep -w VERSION .updated | head -n 1 | cut -d= -f2 | sed 's/"//g')
+	    version_new=$(grep -w VERSION .updated | tail -n 1 | cut -d= -f2 | sed 's/"//g')
+	    echo "${d_rel} updated: ${version_old} --> ${version_new}"
+	fi
+    done
+done<${backports_file}
+
+cleanup
